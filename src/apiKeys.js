d78f882db2f7ac301eeabd3aaa82d8d51792f7b0
Merugu Vinodh Kumar <vinodhkumarmerugu@gmail.com>
2023-09-14 19:32:25 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "414e0cdd9f5992e5bffa4c1e4c54a245",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
